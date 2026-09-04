76598fb167bd59072b793cde29f3cf72ee939c18
Solcafffff <a-loginov@school1409.ru>
2026-09-04 18:24:34 +0300

    pass current_user.login as name to home1.html

--- api/accounts.py
+++ api/accounts.py
@@ -1,39 +1,33 @@
-from main import *
-from db_manager import *
-import requests
+from flask import render_template, redirect, url_for, request, jsonify
+from flask_login import login_required, current_user, login_user
+import requests as http_requests
 import re
 import json
-from flask import jsonify, render_template, request, redirect, url_for
 from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, login_manager, current_user
+from db_manager import *
 
 
+def register_accounts(app, bcrypt, login_manager):
 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return None
 
-#Контроль доступа
-bcrypt = Bcrypt(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+    @app.route("/")
+    def index():
+        return redirect(url_for("login"))
 
+    @app.route("/home")
+    @login_required
+    def home():
+        if not current_user.is_confirmed:
+            return render_template("not_dostup.html")
+        return render_template("home1.html", name=current_user.login)
 
+    @app.route("/login", methods=["GET", "POST"])
+    def login():
+        return render_template("login.html")
 
-
-#-----------------------------------   FRONTEND   -----------------------------------
-
-@app.route("/")
-def index():
-    return redirect(url_for("login"))
-
-
-@app.route("/home")
-@login_required
-def home():
-    if not current_user.is_confirmed: return render_template("not_dostup.html")
-    return render_template("home1.html")
-
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
+    @app.route("/register", methods=["GET", "POST"])
+    def register():
+        return render_template("register.html")
