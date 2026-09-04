0c58947857d11948210dec0258de722c0f59c2da
Solcafffff <a-loginov@school1409.ru>
2026-09-04 21:16:56 +0300

    .global

--- config.py
+++ config.py
@@ -9,6 +9,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+
+# База данных (PostgreSQL) #
+DATABASE_URL=os.environ["DATABASE_URL"]
+
+DB_HOST=os.environ["DB_HOST"]
+DB_PORT=os.environ["DB_PORT"]
+DB_NAME=os.environ["DB_NAME"]
+DB_USER=os.environ["DB_USER"]
+DB_PASSWORD=os.environ["DB_PASSWORD"]
+
 # IOT-Ключ для устройств #
 IOT_KEY=os.environ["IOT_KEY"]
 
@@ -16,8 +26,8 @@ IOT_KEY=os.environ["IOT_KEY"]
 # Секретный ключ Flask #
 SECRET_KEY=os.environ["SECRET_KEY"]
 
-# База данных (PostgreSQL) #
-DATABASE_URL=os.environ["DATABASE_URL"]
+
+
 
 # Пароль администратора #
 ADMIN_PASSWORD=os.environ["ADMIN_PASSWORD"]
